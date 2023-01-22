ab0809a58bba8475b74bbb764155ad8c51a37d4b
Tomás Cichero <martoxdlol@gmail.com>
2023-01-22 20:48:55 -0300

    ai functions

---
+++ src/data/data.ts
@@ -0,0 +1,142 @@
+import { openai } from "../aitools/openai"
+import { firestore } from "./config"
+import { generateAuthorSelfDescription, generateAuthorsNamesAndLangs, generateCategoriesTitles, generateCategoryDescription, generatePostContent, generatePostsTitlesFromCategory, generateSlug, generateSlugFromTitle, generateSummary, generateTopics, matchTitleToAuthor } from "./generators"
+
+interface DocData {
+    id: string // Firebae generated id
+    _created: number // Internal use
+    _updated?: number // Internal use
+}
+
+export interface Category extends DocData {
+    name: string
+    description: string
+    picture: string
+}
+
+export interface Author extends DocData {
+    name: string
+    picture: string
+    lang: string
+    topics: string[]
+}
+
+export interface Post extends DocData {
+    slug: string // Single language
+    lang: string // Default language (original), based on author
+    picture: string
+    title: string
+    summary: string
+    content: string
+    author: string // Ficitcious author
+    date: number // Date of the event/new/post
+}
+
+const MAX_CATEGORIES = parseInt(import.meta.env.MAX_CATEGORIES || 20)
+const MAX_AUTHORS = parseInt(import.meta.env.MAX_AUTHORS || 20)
+const MAX_POST_PER_CATEGORY = parseInt(import.meta.env.MAX_POST_PER_CATEGORY || 20)
+
+
+function docsOf<T extends DocData>(queryResult: FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>) {
+    return queryResult.docs.map(doc => docOf<T>(doc)!)
+}
+
+
+function docOf<T extends DocData>(docResult: FirebaseFirestore.DocumentSnapshot<FirebaseFirestore.DocumentData>) {
+    if (!docResult.exists) return null
+    const doc: any = docResult.data()
+    doc._created = docResult.createTime
+    doc._updated = docResult.updateTime
+    doc.id = docResult.id
+    return doc as T
+}
+
+// Authors
+export async function getCategories(): Promise<Category[]> {
+    const categories = docsOf<Category>(await firestore.collection('categories').get())
+
+    if (categories.length < MAX_CATEGORIES) {
+        const newTitles = await generateCategoriesTitles(categories, MAX_CATEGORIES)
+        for (const title of newTitles) {
+            const description = await generateCategoryDescription(title)
+            const slug = await generateSlugFromTitle(title)
+            const result = await firestore.collection("categories").add({ title, description, slug })
+            categories.push(docOf(await firestore.collection('categories').doc(result.id).get())!)
+        }
+    }
+
+    return categories
+}
+
+export async function getCategory(id: string, lang: string) {
+    return docOf<Category>(await firestore.collection('categories').doc(id).get())
+}
+
+
+// Authors
+export async function getAuthors(): Promise<Author[]> {
+    const authors = docsOf<Author>(await firestore.collection('categories').get())
+
+    if (authors.length < MAX_AUTHORS) {
+        const newAuthors = await generateAuthorsNamesAndLangs(authors, MAX_CATEGORIES)
+        for (const author of newAuthors) {
+            const description = await generateAuthorSelfDescription(author.name, author.lang)
+            const allTopics = await generateTopics()
+            const topics = chooseRandoms(allTopics, random(2, 3))
+            const result = await firestore.collection("authors").add({ lang: author.lang, name: author.name, description, topics })
+            authors.push(docOf(await firestore.collection('authors').doc(result.id).get())!)
+        }
+    }
+
+    return authors
+}
+
+export async function getAuthor(id: string) {
+    return docOf<Author>(await firestore.collection('authors').doc(id).get())
+}
+
+
+// Posts
+export async function getPosts(category: string): Promise<Post[]> {
+    const categoryDoc = docOf<Category>(await firestore.collection('categories').doc(category).get())
+    if (!categoryDoc) return []
+    const posts = docsOf<Post>(await firestore.collection('posts').where('category', '==', category).get())
+
+    if (posts.length < MAX_POST_PER_CATEGORY) {
+        const newPostsTitles = await generatePostsTitlesFromCategory(posts, categoryDoc.name, MAX_POST_PER_CATEGORY)
+        for (const title of newPostsTitles) {
+            const author = await matchTitleToAuthor(await getAuthors(), title)
+            const content = await generatePostContent(author, title, categoryDoc.name, random(400, 700), random(700, 2300))
+            const summary = await generateSummary(title, content)
+            const lang = author.lang
+            const slug = await generateSlug(title)
+            const result = await firestore.collection("posts").add({ title, author, lang, summary, content, slug })
+            posts.push(docOf<Post>(await firestore.collection('posts').doc(result.id).get())!)
+        }
+    }
+    
+    return posts
+}
+
+export async function getPost(id: string, lang: string) {
+    return docOf<Post>(await firestore.collection('posts').doc(id).get())
+}
+
+
+
+function random(mn: number, mx: number) {
+    return Math.random() * (mx - mn) + mn;
+}
+
+function chooseRandoms(list: string[], amount: number) {
+    let l = [...list]
+    const r = []
+    for (let i = 0; i < amount; i++) {
+        let ran = random(0, l.length)
+        r.push(l[ran])
+        l = l.filter((_, i) => {
+            i != ran
+        })
+    }
+    return r
+}
---
+++ src/data/generators.ts
@@ -0,0 +1,148 @@
+import { openai } from "../aitools/openai";
+import type { Author, Category, Post } from "./data";
+
+async function autocomplete(prompt: string, maxTokens: number) {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+        temperature: 0.7,
+        max_tokens: 40 * maxTokens,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    })
+
+    return (response.data.choices[0].text || '').trim()
+}
+
+function separateList(text: string) {
+    let lines = text.split('\n')
+    lines = lines.map(line => line.replace(/^(([0-9]+\. *)|([0-9]+ *\- *)|( *\- *))/gi, ''))
+    lines = lines.map(line => {
+        if (/^".*"$/.test(line)) return line.substring(1, line.length - 1)
+        return line
+    })
+    return lines
+}
+
+export async function generateCategoriesTitles(initialCategories: Category[], maxAmount: number) {
+    const newCategoriesAmount = maxAmount - initialCategories.length
+
+    let prompt = 'This is a list of topics:\n'
+
+    const topics = await generateTopics(Math.floor(maxAmount * 2.5))
+
+    for (const topic of topics) {
+        prompt += '\n-' + topic
+    }
+
+    prompt += `\n\nGenerate 10 blog categories based on the topics:\n`
+    for (const category of initialCategories) {
+        prompt += '\n-' + category.name
+    }
+
+    // Generate categories
+    const result = await autocomplete(prompt, 40 * newCategoriesAmount)
+
+    return separateList(result)
+}
+
+export function generateCategoryDescription(title: string) {
+    const prompt = "part of my general knowledge blog.\n"
+        + 'Category: "' + title + '"\n'
+        + "Description for SEO (250 characters long):"
+    return autocomplete(prompt, 300)
+}
+
+export function generateSlugFromTitle(title: string) {
+    const prompt = "Task: create slug for this title: \n" + title
+        + '---'
+        + "slug:"
+    return autocomplete(prompt, 100)
+}
+
+export async function generateAuthorsNamesAndLangs(initialAuthors: Author[], maxAmount: number) {
+    const newAuthorsAmount = maxAmount - initialAuthors.length
+
+    let prompt = `Task: list ${maxAmount} names from different nationalities and say the native speaking language\n`
+    prompt += 'Format: [language code]: [First Name] [Last Name]\n---'
+    for (const author of initialAuthors) {
+        prompt += `\n${author.lang}:${author.name}`
+    }
+
+    // Generate categories
+    const result = await autocomplete(prompt, 40 * newAuthorsAmount)
+
+    return result.split('\n').map(r => {
+        const arr = r.split(':')
+        return {
+            name: arr[1]!,
+            lang: arr[0]
+        }
+    })
+}
+
+let topics: string[] | null = null
+
+export async function generateTopics(limit: number = 30) {
+    if (topics) return topics
+    return separateList(await autocomplete(`List ${limit} general topics of blogs and news:`, limit * 40))
+}
+
+export async function matchTitleToAuthor(authors: Author[], title: string) {
+    let prompt = `These are authors from my blog and the topics each one writes about
+    Format: [language code]: [First Name] [Last Name] - [Topics of the author]`
+    let i = 1
+    for (const author of authors) {
+        prompt += `\n${i}. ${author.lang}:${author.name} - ${author.topics.join(',')}`
+        i++
+    }
+    prompt += '\n\nMatch one of above with this blog post title "The Evolution of Dance Through the Ages" considering the correct topic:\nNumber: '
+    const result = await autocomplete(prompt, 10)
+    return authors[parseInt(result) - 1]
+}
+
+export async function generateAuthorSelfDescription(name: string, lang: string) {
+    const prompt = `Task: make a 300 characters self description
+    Name: ${name}
+    Output language: ${lang}
+    ---`
+    return await autocomplete(prompt, 400)
+}
+
+export async function generatePostsTitlesFromCategory(initialPosts: Post[], categoryName: string, maxAmount: number) {
+    let prompt = `These are 10 titles of my general knowledge blog posts in the category named "${categoryName}"
+    Be interesting
+    Do not repeat "${categoryName}" 
+    ---`
+    for (const post of initialPosts) {
+        prompt += `\n- ${post.title}`
+    }
+    return separateList(await autocomplete(prompt, maxAmount * 40))
+}
+
+export async function generatePostContent(author: Author, title: string, categoryName: string, minLength: number, maxLength: number) {
+    return await autocomplete(`Generate a blog post content in markdown and informal language considering:
+    Post title: "${title}"
+    Author name: "${author.lang}"
+    Language: "${author.lang}"
+    Blog category: "${categoryName}"
+    Min length: ${minLength} characters
+    Max length: ${maxLength} characters
+    
+    Include a conclusión and add sources
+    ---`, maxLength + 100)
+}
+
+export async function generateSummary(title: string, text: string) {
+    return await autocomplete(`Title: "${title}"
+    
+    ${text}
+
+    Summary optimized for SEO (300 characters):`, 400)
+}
+
+export async function generateSlug(title: string) {
+    return (await autocomplete(`Title: "${title}"
+slug:`, 400)).split('\n').filter(l => l.trim() != '')[0].trim()
+}
